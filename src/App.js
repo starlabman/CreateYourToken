b9ef1ce8734eedc19077f6705fa93b1c9c893edc
labore <akodjolabore@gmail.com>
2022-02-01 12:06:59 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {useState, useEffect } from 'react';
+import {ethers} from 'ethers';
+import Token from './artifacts/contracts/Token.sol/Token.json';
+import './App.css';
+
+const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+function App() {
+
+  const [balance, setBalance] = useState();
+
+  useEffect(() => {
+    getBalance();
+  }, [])
+
+  async function getBalance() {
+    if(typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(accounts[0]);
+      setBalance(balance / 10**18);
+    }
+  }
+
+  return (
+    <div className="App">
+      <p>Vous avez {balance} LBC</p>
+    </div>
+  );
+}
+
+export default App;
